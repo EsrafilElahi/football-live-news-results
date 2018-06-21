3911dafec152e9cce8c5fef36958dd249e412b1e
esrafil elahi <esrafil.elahi@gmail.com>
2018-06-21 15:03:35 +0430

    fix ec league

--- pages/ec/index.js
+++ pages/ec/index.js
@@ -1,68 +1,67 @@
-import axios from 'axios'
-import Layout from '../../components/layout/Layout'
-import { MatchCard } from './../../components/other/MatchCard'
-import Pagination from './../../components/other/Pagination'
-import usePaginationTools from './../../components/pagination tools/usePaginationTools'
-
+import axios from "axios";
+import Layout from "../../components/layout/Layout";
+import { MatchCard } from "./../../components/other/MatchCard";
+import Pagination from "./../../components/other/Pagination";
+import usePaginationTools from "./../../components/pagination tools/usePaginationTools";
 
 function EC({ data }) {
+  const posts = data.matches;
+  const { paginatedPosts, paginate, postsPerPage } = usePaginationTools(posts);
 
-    const posts = data.matches
-    const { paginatedPosts, paginate, postsPerPage } = usePaginationTools(posts)
+  return (
+    <Layout alertTitle="EURO 2020">
+      <div className="row gy-3 content-sec">
+        {posts.length === 0 ? (
+          <div className="mt-4 text-danger">There Is No Match 🤔</div>
+        ) : (
+          paginatedPosts.map((post) => {
+            const Hteam = post.homeTeam.name;
+            const Ateam = post.awayTeam.name;
+            const Hscore = post.score.fullTime.homeTeam;
+            const Ascore = post.score.fullTime.awayTeam;
 
-    return (
-        <Layout alertTitle='EURO 2020'>
-            <div className='row gy-3 content-sec'>
-
-                {
-                    posts.length === 0 ? <div className='mt-4 text-danger'>There Is No Match 🤔</div> :
-                        paginatedPosts.map((post) => {
-                            const Hteam = post.homeTeam.name
-                            const Ateam = post.awayTeam.name
-                            const Hscore = post.score.fullTime.homeTeam
-                            const Ascore = post.score.fullTime.awayTeam
-
-                            return (
-                                <div key={post.id} className='col-xs-12 col-lg-6'>
-                                    <MatchCard 
-                                        id={post.id} 
-                                        League={'ec'} 
-                                        Hteam={Hteam} 
-                                        Hscore={Hscore} 
-                                        Ateam={Ateam} 
-                                        Ascore={Ascore} 
-                                    />
-                                </div>
-                            )
-                        })
-                }
-
-                <Pagination
-                    postsPerPage={postsPerPage}
-                    totalPosts={posts.length}
-                    paginate={paginate}
+            return (
+              <div key={post.id} className="col-xs-12 col-lg-6">
+                <MatchCard
+                  id={post.id}
+                  League={"ec"}
+                  Hteam={Hteam}
+                  Hscore={Hscore}
+                  Ateam={Ateam}
+                  Ascore={Ascore}
                 />
+              </div>
+            );
+          })
+        )}
 
-            </div>
-        </Layout>
-    )
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={posts.length}
+          paginate={paginate}
+        />
+      </div>
+    </Layout>
+  );
 }
-
 
 export const getServerSideProps = async () => {
-    const res = await axios.get(`https://api.football-data.org/v2/competitions/EC/matches?status=FINISHED`, {
-        headers: { 'X-Auth-Token': '24574cf932a34d28b394c721600f5471' }
-    })
-    const data = res.data
-
-    if (!data) {
-        return {
-            notFound: true
-        }
+  const res = await axios.get(
+    `https://api.football-data.org/v2/competitions/EC/matches?status=FINISHED`,
+    {
+      headers: { "X-Auth-Token": "24574cf932a34d28b394c721600f5471" },
     }
+  );
+  const data = res.data;
+
+  if (!data) {
     return {
-        props: { data }
-    }
-}
+      notFound: true,
+    };
+  }
+  return {
+    props: { data },
+  };
+};
 
-export default EC
+export default EC;
