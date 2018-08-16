08a952d58565ad06d98ba48223a1dcf19b82d2b6
esrafil elahi <esrafil.elahi@gmail.com>
2018-08-16 15:03:21 +0430

    fix Serie A league bugs

--- pages/serieA/index.js
+++ pages/serieA/index.js
@@ -1,68 +1,70 @@
-import axios from 'axios'
-import Layout from '../../components/layout/Layout'
-import { MatchCard } from './../../components/other/MatchCard'
-import Pagination from './../../components/other/Pagination'
-import usePaginationTools from './../../components/pagination tools/usePaginationTools'
-
+import axios from "axios";
+import Layout from "../../components/layout/Layout";
+import { MatchCard } from "./../../components/other/MatchCard";
+import Pagination from "./../../components/other/Pagination";
+import usePaginationTools from "./../../components/pagination tools/usePaginationTools";
 
 function SerieA({ data }) {
+  const posts = data.matches;
+  const { paginatedPosts, paginate, postsPerPage } = usePaginationTools(posts);
 
-    const posts = data.matches
-    const { paginatedPosts, paginate, postsPerPage } = usePaginationTools(posts)
+  return (
+    <Layout alertTitle="Serie A">
+      <div className="row gy-3 content-sec">
+        {posts.length === 0 ? (
+          <div className="mt-4 text-danger">There Is No Match 🤔</div>
+        ) : (
+          paginatedPosts
+            .slice(0)
+            .reverse()
+            .map((post) => {
+              const Hteam = post.homeTeam.name;
+              const Ateam = post.awayTeam.name;
+              const Hscore = post.score.fullTime.homeTeam;
+              const Ascore = post.score.fullTime.awayTeam;
 
-    return (
-        <Layout alertTitle='Serie A'>
-            <div className='row gy-3 content-sec'>
+              return (
+                <div key={post.id} className="col-xs-12 col-lg-6">
+                  <MatchCard
+                    id={post.id}
+                    League={"serieA"}
+                    Hteam={Hteam}
+                    Hscore={Hscore}
+                    Ateam={Ateam}
+                    Ascore={Ascore}
+                  />
+                </div>
+              );
+            })
+        )}
 
-                {
-                    posts.length === 0 ? <div className='mt-4 text-danger'>There Is No Match 🤔</div> :
-                        paginatedPosts.slice(0).reverse().map((post) => {
-                            const Hteam = post.homeTeam.name
-                            const Ateam = post.awayTeam.name
-                            const Hscore = post.score.fullTime.homeTeam
-                            const Ascore = post.score.fullTime.awayTeam
-
-                            return (
-                                <div key={post.id} className='col-xs-12 col-lg-6'>
-                                    <MatchCard 
-                                        id={post.id} 
-                                        League={'serieA'} 
-                                        Hteam={Hteam} 
-                                        Hscore={Hscore} 
-                                        Ateam={Ateam} 
-                                        Ascore={Ascore} 
-                                    />
-                                </div>
-                            )
-                        })
-                }
-
-                <Pagination
-                    postsPerPage={postsPerPage}
-                    totalPosts={posts.length}
-                    paginate={paginate}
-                />
-
-            </div>
-        </Layout>
-    )
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={posts.length}
+          paginate={paginate}
+        />
+      </div>
+    </Layout>
+  );
 }
-
 
 export const getServerSideProps = async () => {
-    const res = await axios.get(`https://api.football-data.org/v2/competitions/SA/matches?status=FINISHED`, {
-        headers: { 'X-Auth-Token': '24574cf932a34d28b394c721600f5471' }
-    })
-    const data = res.data
-
-    if (!data) {
-        return {
-            notFound: true
-        }
+  const res = await axios.get(
+    `https://api.football-data.org/v2/competitions/SA/matches?status=FINISHED`,
+    {
+      headers: { "X-Auth-Token": "24574cf932a34d28b394c721600f5471" },
     }
+  );
+  const data = res.data;
+
+  if (!data) {
     return {
-        props: { data }
-    }
-}
+      notFound: true,
+    };
+  }
+  return {
+    props: { data },
+  };
+};
 
-export default SerieA
+export default SerieA;
