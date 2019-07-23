e5fd15a41c794171b445b7138c58c0eb12cf6a96
esrafil elahi <esrafil.elahi@gmail.com>
2019-07-23 15:17:16 +0430

    add Laliga page

--- pages/mrGoal/Laliga/index.js
+++ pages/mrGoal/Laliga/index.js
@@ -1,67 +1,71 @@
-import { useContext } from 'react'
-import axios from 'axios'
-import Layout from './../../../components/layout/Layout'
-import { ThemeContext } from './../../../components/context api/ThemeContext'
-
+import { useContext } from "react";
+import axios from "axios";
+import Layout from "./../../../components/layout/Layout";
+import { ThemeContext } from "./../../../components/context api/ThemeContext";
 
 function Laliga({ dataPD }) {
+  const { darkMode } = useContext(ThemeContext);
+  const posts = dataPD.scorers;
 
-    const { darkMode } = useContext(ThemeContext)
-    const posts = dataPD.scorers
-
-    return (
-
-        <Layout alertTitle='Laliga Scorers'>
-            <div className='row gy-3 content-sec'>
-
-                {posts.length === 0 ? <div className='mt-4 text-danger'>There Is No Scorers 🤔</div> :
-                    <table className={darkMode ? "standing-darki table table-hover" : "standing-lighti table table-hover"}>
-                        <thead>
-                            <tr>
-                                <th scope="col">position</th>
-                                <th scope="col">name</th>
-                                <th scope="col">team</th>
-                                <th scope="col">goals</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                posts.map((post, index) => {
-                                    let position = index + 1
-                                    return (
-                                        <tr key={index}>
-                                            <th scope="row">{position}</th>
-                                            <td>{post.player.name}</td>
-                                            <td>{post.team.name}</td>
-                                            <td>{post.numberOfGoals}</td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
-                }
-
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout alertTitle="Laliga Scorers">
+      <div className="row gy-3 content-sec">
+        {posts.length === 0 ? (
+          <div className="mt-4 text-danger">There Is No Scorers 🤔</div>
+        ) : (
+          <table
+            className={
+              darkMode
+                ? "standing-darki table table-hover"
+                : "standing-lighti table table-hover"
+            }
+          >
+            <thead>
+              <tr>
+                <th scope="col">position</th>
+                <th scope="col">name</th>
+                <th scope="col">team</th>
+                <th scope="col">goals</th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.map((post, index) => {
+                let position = index + 1;
+                return (
+                  <tr key={index}>
+                    <th scope="row">{position}</th>
+                    <td>{post.player.name}</td>
+                    <td>{post.team.name}</td>
+                    <td>{post.numberOfGoals}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </Layout>
+  );
 }
 
 export const getServerSideProps = async () => {
-    const scorersResPD = await axios.get(`https://api.football-data.org/v2/competitions/PD/scorers`, {
-        headers: { 'X-Auth-Token': '24574cf932a34d28b394c721600f5471' }
-    })
-
-    const dataPD = scorersResPD.data
-
-    if (!dataPD) {
-        return {
-            notFound: true
-        }
+  const scorersResPD = await axios.get(
+    `https://api.football-data.org/v2/competitions/PD/scorers`,
+    {
+      headers: { "X-Auth-Token": "24574cf932a34d28b394c721600f5471" },
     }
+  );
+
+  const dataPD = scorersResPD.data;
+
+  if (!dataPD) {
     return {
-        props: { dataPD }
-    }
-}
+      notFound: true,
+    };
+  }
+  return {
+    props: { dataPD },
+  };
+};
 
-export default Laliga
+export default Laliga;
