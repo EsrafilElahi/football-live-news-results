3a757921722d678eceff9a205bd553aa34694166
esrafil elahi <esrafil.elahi@gmail.com>
2019-09-24 15:17:18 +0330

    standing page added

--- pages/standing/index.js
+++ pages/standing/index.js
@@ -1,23 +1,43 @@
-import Layout from '../../components/layout/Layout'
-import StandingCard from './../../components/other/StandingCard'
-
+import Layout from "../../components/layout/Layout";
+import StandingCard from "./../../components/other/StandingCard";
 
 function Standing() {
-
-    return (
-        <Layout alertTitle='Which League Standing ?'>
-            <div className='row gx-1 gy-3 content-sec'>
-
-                <StandingCard country='england' league='Premier-League' src='/images/premier-league.png' />
-                <StandingCard country='spain' league='Laliga' src='/images/laliga.jpg' />
-                <StandingCard country='italy' league='Serie-A' src='/images/seriea.png' />
-                <StandingCard country='germany' league='Bundesliga' src='/images/bundesliga.jpg' />
-                <StandingCard country='france' league='Ligue-1' src='/images/ligue1.jpg' />
-                <StandingCard country='UEFA Champions League' league='UCL' src='/images/ucl.jpg' />
-
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout alertTitle="Which League Standing ?">
+      <div className="row gx-1 gy-3 content-sec">
+        <StandingCard
+          country="england"
+          league="Premier-League"
+          src="/images/premier-league.png"
+        />
+        <StandingCard
+          country="spain"
+          league="Laliga"
+          src="/images/laliga.jpg"
+        />
+        <StandingCard
+          country="italy"
+          league="Serie-A"
+          src="/images/seriea.png"
+        />
+        <StandingCard
+          country="germany"
+          league="Bundesliga"
+          src="/images/bundesliga.jpg"
+        />
+        <StandingCard
+          country="france"
+          league="Ligue-1"
+          src="/images/ligue1.jpg"
+        />
+        <StandingCard
+          country="UEFA Champions League"
+          league="UCL"
+          src="/images/ucl.jpg"
+        />
+      </div>
+    </Layout>
+  );
 }
 
-export default Standing
+export default Standing;
